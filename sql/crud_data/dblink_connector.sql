6623b3930691bc1348fef8684279de578890446c
MonadWizard <monad.wizard.r@gmail.com>
2025-04-09 15:55:31 +0600

    add test for read and wight

--- sql/crud_data/dblink_connector.sql
+++ sql/crud_data/dblink_connector.sql
@@ -89,14 +89,17 @@ $$);
 
 
 
-CREATE OR REPLACE FUNCTION get_data_as_jsonb(passing_data jsonb)
+CREATE OR REPLACE FUNCTION get_data_as_jsonb_dblink(passing_data jsonb)
 RETURNS jsonb AS $$
 DECLARE
     result jsonb;
 
     database_conn jsonb := passing_data ->> 'database_conn';
+    conn_name text := database_conn ->> 'conn';
+
     user_id varchar := passing_data ->> 'user_id';
     conn boolean;
+    query text;
 
 BEGIN
     -- Connect to the source database
@@ -107,9 +110,15 @@ BEGIN
         RETURN '[]'::jsonb; -- Return an empty JSON array in case of connection failure
     end if;
 
-    EXECUTE 'SELECT row_to_json(t) FROM (SELECT user_fullname, user_email, user_primary_pic FROM auth_user_app_user WHERE userid = '''|| user_id ||''') t'
-    INTO result
-    USING user_id;
+--     query:= format('SELECT row_to_json(t) FROM (SELECT user_fullname, user_email, user_primary_pic FROM auth_user_app_user WHERE userid = %L) t', user_id);
+    query:= format('SELECT user_fullname, user_email, user_primary_pic FROM auth_user_app_user WHERE userid = %L', user_id);
+
+    -- Execute query remotely using dblink
+        SELECT row_to_json(row) INTO result
+            FROM dblink(conn_name, query)
+                 AS row(user_fullname jsonb, user_email varchar, user_primary_pic varchar);
+
+--     EXECUTE query INTO result;
 
     -- Return the result as jsonb
     RETURN result;
@@ -122,7 +131,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-explain analyse select get_data_as_jsonb($$
+explain analyse select get_data_as_jsonb_dblink($$
     {
     "database_conn" : {
           "conn": "pg4_conn",
@@ -140,4 +149,84 @@ $$);
 explain analyze SELECT row_to_json(t) FROM (SELECT user_fullname, user_email, user_primary_pic FROM auth_user_app_user WHERE userid = 'BD__0126103817299479') t;
 
 
-select * from auth_user_app_user where userid = 'BD__0126103817299479';
+select user_fullname, user_email, user_primary_pic from auth_user_app_user where userid = 'BD__0126103817299479';
+
+
+
+
+
+
+
+
+--    --   write operation ----------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION update_data_as_jsonb_dblink(passing_data jsonb)
+RETURNS jsonb AS $$
+DECLARE
+
+    database_conn jsonb := passing_data ->> 'database_conn';
+    conn_name text := database_conn ->> 'conn';
+    user_id varchar := passing_data ->> 'user_id';
+    conn boolean;
+
+    user_fullname jsonb := passing_data ->> 'user_fullname';
+    user_email varchar := passing_data ->> 'user_email';
+    user_primary_pic varchar := passing_data ->> 'user_primary_pic';
+    query text;
+
+BEGIN
+    -- Connect to the source database
+    conn := (select connect_to_pg(database_conn));
+
+    if conn is false then
+        RAISE NOTICE 'Connection failed';
+        RETURN '[]'::jsonb; -- Return an empty JSON array in case of connection failure
+    end if;
+
+    query:= format('UPDATE auth_user_app_user set user_fullname=%L,user_email=%L,user_primary_pic=%L  WHERE userid = %L',user_fullname,user_email,user_primary_pic, user_id);
+
+    raise notice 'Query:::::::::::::: %', query;
+
+    PERFORM dblink_exec(conn_name, query);
+
+    -- Return the result as jsonb
+    RETURN jsonb_build_object('STATUS', 'SUCCESS');
+
+EXCEPTION
+    WHEN others THEN
+        RAISE NOTICE 'Error fetching data: %', SQLERRM;
+        RETURN jsonb_build_object('STATUS', 'FAIL');
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+explain analyse select update_data_as_jsonb_dblink($$
+    {
+    "database_conn" : {
+          "conn": "pg4_conn",
+          "host": "192.168.0.215",
+          "port": "5436",
+          "dbname": "aggamedb",
+          "user": "aggame_dev",
+          "password": "L:>(6]nkYw8se&)UQ{"
+        },
+    "user_id": "BD__0126103817299479",
+
+    "user_fullname" : {"last_name": "change dblink ", "first_name": "Player1"},
+    "user_email" : "player1@ag.com",
+    "user_primary_pic" : "baal baal black ship"
+
+    }
+$$);
+
+
+EXPLAIN ANALYSE
+UPDATE auth_user_app_user set user_fullname='{"last_name": "change dblink ", "first_name": "Player1"}',user_email='player1@ag.com',user_primary_pic='baal baal black ship'  WHERE userid = 'BD__0126103817299479';
+
+
+select user_fullname, user_email, user_primary_pic from auth_user_app_user where userid = 'BD__0126103817299479';
+
+
+
--- sql/crud_data/postgres_fdw_connector.sql
+++ sql/crud_data/postgres_fdw_connector.sql
@@ -3,7 +3,7 @@
 DROP USER MAPPING IF EXISTS FOR current_user SERVER pg4_server;
 
 -- Drop the foreign server if it exists
-DROP SERVER IF EXISTS pg1_server CASCADE;
+DROP SERVER IF EXISTS pg4_server CASCADE;
 
 -- Creating the foreign server for pg1
 CREATE SERVER pg4_server
@@ -17,11 +17,12 @@ OPTIONS (user 'aggame_dev', password 'L:>(6]nkYw8se&)UQ{');
 
 
 
+drop foreign table if exists auth_user_app_user_pg1;
 
 -- Create foreign table for auth_user_app_user on pg1
 CREATE FOREIGN TABLE auth_user_app_user_pg1 (
     userid varchar,
-    user_fullname varchar,
+    user_fullname jsonb,
     user_email varchar,
     user_primary_pic bytea
 )
@@ -31,6 +32,8 @@ OPTIONS (schema_name 'public', table_name 'auth_user_app_user');
 
 
 
+
+--    read operation ----------------------------------------------------------------------------------------------------------------
 
 
 CREATE OR REPLACE FUNCTION get_data_as_jsonb(passing_data jsonb)
@@ -67,3 +70,57 @@ FROM (
     FROM auth_user_app_user_pg1
     WHERE userid = 'BD__0126103817299479'
 ) t;
+
+
+
+
+
+
+
+--  --    write operation ----------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION update_data_as_jsonb(passing_data jsonb)
+RETURNS jsonb AS $$
+DECLARE
+    user_fullname jsonb := passing_data ->> 'user_fullname';
+    user_email varchar := passing_data ->> 'user_email';
+    user_primary_pic varchar := passing_data ->> 'user_primary_pic';
+    user_id varchar := passing_data ->> 'user_id';
+    query text;
+
+BEGIN
+    -- Query the foreign table directly
+    query:= format('UPDATE auth_user_app_user_pg1 set user_fullname=%L,user_email=%L,user_primary_pic=%L  WHERE userid = %L',user_fullname,user_email,user_primary_pic, user_id);
+
+    raise notice 'Query:::::::::::::: %', query;
+
+    EXECUTE query;
+
+    -- Return the result as jsonb
+    RETURN jsonb_build_object('STATUS', 'SUCCESS');
+
+
+EXCEPTION
+    WHEN others THEN
+        RAISE NOTICE 'Error fetching data: %', SQLERRM;
+        RETURN jsonb_build_object('STATUS', 'FAIL');
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+EXPLAIN ANALYZE
+select update_data_as_jsonb($${
+    "user_fullname" : {"last_name": "change", "first_name": "Player1"},
+    "user_email" : "player1@ag.com",
+  "user_primary_pic" : "baal baal black ship",
+    "user_id" : "BD__0126103817299479"
+}$$);
+
+
+EXPLAIN ANALYZE UPDATE auth_user_app_user set user_fullname='{"last_name": "change gc", "first_name": "Player1"}',user_email='player1@ag.com',user_primary_pic='baal baal black ship'  WHERE userid = 'BD__0126103817299479';
+
+
+
+select user_fullname from auth_user_app_user where userid = 'BD__0126103817299479';
+
